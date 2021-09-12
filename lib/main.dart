9bbcc86ed130584fe177e22a16bf117ca5efb6f6
Lucas Andrade <lucasandrade595@gmail.com>
2021-09-12 14:22:53 -0300

    Feat: Implementando o DashBoard. Feat: Implementando todo fluxo do app.

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //remove canto superior a flag de debug
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )),
+      home: DashBoard(),
+    );
+  }
+}
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,39 @@
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ConctactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conctacts"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                "Lucas",
+                style: TextStyle(fontSize: 24.0),
+              ),
+              subtitle: Text(
+                "1000",
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((newContact) => debugPrint(newContact.toString()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
